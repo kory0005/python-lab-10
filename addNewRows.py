8bf3c894e37f210d0386b680a4182a43b3505df9
kory0005 <kory0005@algonquinlive.com>
2019-11-18 02:09:16 -0600

    initial commit

--- addNewRows.py
+++ addNewRows.py
@@ -7,14 +7,15 @@ conn = sqlite3.connect('week10.db')
 c = conn.cursor()
 
 
-# ADDING NEW ROWS
-c.execute('INSERT INTO lab10 (link,city,country,student) VALUES("aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS9tYXBzL3BsYWNlL0toYXJraXYsK09ibGFzdCtkZStLaGFya2l2LCtVa3JhaW5lL0A0OS45OTQ1MDcsMzYuMTQ1NzQyLDExei9kYXRhPSEzbTEhNGIxITRtNSEzbTQhMXMweDQxMjdhMDlmNjNhYjBmOGI6MHgyZDRjMTg2ODFhYTRiZTBhIThtMiEzZDQ5Ljk5MzUhNGQzNi4yMzAzODM=", "Kharkiv", "Ukraine", "Nonna")')
-c.execute('INSERT INTO lab10 (link,city,country,student) VALUES("aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS9tYXBzL3BsYWNlL0FtaWVucywrRnJhbmNlL0A0OS44OTg1NDA4LDIuMjE0NTk3OCwxMnovZGF0YT0hM20xITRiMSE0bTUhM200ITFzMHg0N2U3ODQxM2Q3OGI3NjBiOjB4NDBhZjEzZTgxNjIyMGUwIThtMiEzZDQ5Ljg5NDA2NyE0ZDIuMjk1NzUz", "Amiens", "France", "Jerome")')
-c.execute('INSERT INTO lab10 (link,city,country,student) VALUES("aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS9tYXBzL3BsYWNlL0lzcGFoYW4sK0lyYW4vQDMyLjY2MjIxMDcsNTEuNTQ2NTk3NCwxMXovZGF0YT0hM20xITRiMSE0bTUhM200ITFzMHgzZmJjMzVmZThjMzI2Nzk5OjB4N2FiNTc4MTZlZjU4MzdmNSE4bTIhM2QzMi42NTM4OTY2ITRkNTEuNjY1OTY1Ng==", "Isfahan", "Iran", "Saba")')
-c.execute('INSERT INTO lab10 (link,city,country,student) VALUES("aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS9tYXBzL3BsYWNlL0lzcGFoYW4sK0lyYW4vQDMyLjY2MjIxMDcsNTEuNTQ2NTk3NCwxMXovZGF0YT0hM20xITRiMSE0bTUhM200ITFzMHgzZmJjMzVmZThjMzI2Nzk5OjB4N2FiNTc4MTZlZjU4MzdmNSE4bTIhM2QzMi42NTM4OTY2ITRkNTEuNjY1OTY1Ng==", "Lviv", "Ukraine", "Illya")')
-c.execute('INSERT INTO lab10 (link,city,country,student) VALUES("aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS9tYXBzL3BsYWNlL0lzcGFoYW4sK0lyYW4vQDMyLjY2MjIxMDcsNTEuNTQ2NTk3NCwxMXovZGF0YT0hM20xITRiMSE0bTUhM200ITFzMHgzZmJjMzVmZThjMzI2Nzk5OjB4N2FiNTc4MTZlZjU4MzdmNSE4bTIhM2QzMi42NTM4OTY2ITRkNTEuNjY1OTY1Ng==", "Gold Coast", "Australia", " ")')
-conn.commit()
-# print("Record Updated successfully ")
+# CREATE NEW ROW
+while True:
+    link = input('Link in base64 format: ')
+    city = input('City: ')
+    country = input('Country: ')
+    student = input('Student: ')
+    c.execute("INSERT INTO lab10 (link,city,country,student) VALUES('{}','{}','{}','{}')".format(link, city, country, student))
+    print("Record Added Successfully!")
+    conn.commit()
 
 c.close()
 conn.close()
